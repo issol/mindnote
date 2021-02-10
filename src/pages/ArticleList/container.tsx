2224f66d8a2e472f244b4731b42c7060a427e2a6
issol <isolatorv@gmail.com>
2021-02-10 15:51:40 +0900

    fix/ modal component 분리

--- src/pages/ArticleList/container.tsx
+++ src/pages/ArticleList/container.tsx
@@ -5,68 +5,68 @@ import { useHistory } from 'react-router-dom';
 import { RootState } from 'store';
 
 import {
-  createArticle,
-  deleteArticle,
-  fetchArticleList,
+    createArticle,
+    deleteArticle,
+    fetchArticleList,
 } from '../../store/articles/actions';
 import ArticleListPresenter from './presenter';
 
 const ArticleListContainer = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const [isOpenCreateArticleModal, setIsOpenCreateArticleModal] = useState(
-    false
-  );
-  const { register, handleSubmit, errors } = useForm();
-  const dispatch = useDispatch();
-  const history = useHistory();
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [isOpenCreateArticleModal, setIsOpenCreateArticleModal] = useState(
+        false
+    );
+    const { register, handleSubmit, errors } = useForm();
+    const dispatch = useDispatch();
+    const history = useHistory();
 
-  const articleReducer = useSelector(
-    (state: RootState) => state.articleReducer
-  );
+    const articleReducer = useSelector(
+        (state: RootState) => state.articleReducer
+    );
 
-  const handleCreateArticle = (data: any) => {
-    setIsOpenCreateArticleModal(false);
-    dispatch(createArticle.request(data));
-  };
+    const handleCreateArticle = (data: any) => {
+        setIsOpenCreateArticleModal(false);
+        dispatch(createArticle.request(data));
+    };
 
-  const handleDeleteArticle = (e: any) => {
-    dispatch(deleteArticle.request(e.target.id));
-  };
+    const handleDeleteArticle = (e: any) => {
+        dispatch(deleteArticle.request(e.target.id));
+    };
 
-  const openModalForCreateArticle = () => {
-    setIsOpenCreateArticleModal(true);
-  };
+    const openModalForCreateArticle = () => {
+        setIsOpenCreateArticleModal(true);
+    };
 
-  useEffect(() => {
-    if (localStorage.getItem('token') === null) {
-      setIsLoggedIn(false);
-    } else {
-      setIsLoggedIn(true);
-    }
-  }, []);
+    useEffect(() => {
+        if (localStorage.getItem('token') === null) {
+            setIsLoggedIn(false);
+        } else {
+            setIsLoggedIn(true);
+        }
+    }, []);
 
-  useEffect(() => {
-    dispatch(fetchArticleList.request());
-  }, [articleReducer.articleList]);
+    useEffect(() => {
+        dispatch(fetchArticleList.request());
+    }, [dispatch, fetchArticleList]);
 
-  return (
-    <>
-      {isLoggedIn ? (
-        <ArticleListPresenter
-          isOpenCreateArticleModal={isOpenCreateArticleModal}
-          openModalForCreateArticle={openModalForCreateArticle}
-          articleList={articleReducer.articleList}
-          handleCreateArticle={handleCreateArticle}
-          handleDeleteArticle={handleDeleteArticle}
-          register={register}
-          handleSubmit={handleSubmit}
-          errors={errors}
-        />
-      ) : (
-        history.push('/')
-      )}
-    </>
-  );
+    return (
+        <>
+            {isLoggedIn ? (
+                <ArticleListPresenter
+                    isOpenCreateArticleModal={isOpenCreateArticleModal}
+                    openModalForCreateArticle={openModalForCreateArticle}
+                    articleList={articleReducer.articleList}
+                    handleCreateArticle={handleCreateArticle}
+                    handleDeleteArticle={handleDeleteArticle}
+                    register={register}
+                    handleSubmit={handleSubmit}
+                    errors={errors}
+                />
+            ) : (
+                history.push('/')
+            )}
+        </>
+    );
 };
 
 export default ArticleListContainer;
--- src/store/articles/sagas.ts
+++ src/store/articles/sagas.ts
@@ -1,92 +1,93 @@
 import axios from 'axios';
 import { call, put, takeEvery } from 'redux-saga/effects';
 import {
-  FETCH_ARTICLE_LIST_REQUEST,
-  fetchArticleList,
-  createArticle,
-  CREATE_ARTICLE_REQUEST,
-  deleteArticle,
-  DELETE_ARTICLE_REQUEST,
-  updateArticle,
-  UPDATE_ARTICLE_REQUEST,
+    FETCH_ARTICLE_LIST_REQUEST,
+    fetchArticleList,
+    createArticle,
+    CREATE_ARTICLE_REQUEST,
+    deleteArticle,
+    DELETE_ARTICLE_REQUEST,
+    updateArticle,
+    UPDATE_ARTICLE_REQUEST,
 } from './actions';
 import { HOST } from 'constants/requests';
 import { ArticleInfo, UpdatedArticleInfo } from './types';
 
 const fetchArticleListApi = (token) =>
-  axios.get(HOST + '/articles/my-list/', {
-    headers: { Authorization: `token ${token}` },
-  });
+    axios.get(HOST + '/articles/my-list/', {
+        headers: { Authorization: `token ${token}` },
+    });
 
 function* fetchArticleListAsync() {
-  try {
-    const token = localStorage.getItem('token');
-    const res = yield call(fetchArticleListApi, token);
-    yield put(fetchArticleList.success(res.data));
-  } catch (e) {
-    yield put(fetchArticleList.failure(e.request.responseText));
-  }
+    try {
+        const token = localStorage.getItem('token');
+        const res = yield call(fetchArticleListApi, token);
+        yield put(fetchArticleList.success(res.data));
+    } catch (e) {
+        yield put(fetchArticleList.failure(e.request.responseText));
+    }
 }
 
 const createArticleApi = (token: any, payload: ArticleInfo) =>
-  axios.post(HOST + '/articles/', payload, {
-    headers: { Authorization: `token ${token}` },
-  });
+    axios.post(HOST + '/articles/', payload, {
+        headers: { Authorization: `token ${token}` },
+    });
 
 function* createArticleAsync(action: { type: string; payload: ArticleInfo }) {
-  try {
-    const token = localStorage.getItem('token');
-    const res = yield call(createArticleApi, token, action.payload);
-    yield put(createArticle.success(res.data));
-  } catch (e) {
-    yield put(createArticle.failure(e.request.responseText));
-  }
+    try {
+        const token = localStorage.getItem('token');
+        const res = yield call(createArticleApi, token, action.payload);
+
+        yield put(createArticle.success(res.data));
+    } catch (e) {
+        yield put(createArticle.failure(e.request.responseText));
+    }
 }
 
 const deleteArticleApi = (token: any, id: number) =>
-  axios.delete(HOST + `/articles/${id}/`, {
-    headers: { Authorization: `token ${token}` },
-  });
+    axios.delete(HOST + `/articles/${id}/`, {
+        headers: { Authorization: `token ${token}` },
+    });
 
 function* deleteArticleAsync(action) {
-  try {
-    const token = localStorage.getItem('token');
-    const id = action.payload;
-    const res = yield call(deleteArticleApi, token, id);
+    try {
+        const token = localStorage.getItem('token');
+        const id = action.payload;
+        const res = yield call(deleteArticleApi, token, id);
 
-    yield put(deleteArticle.success(res));
-  } catch (e) {
-    yield put(deleteArticle.failure());
-  }
+        yield put(deleteArticle.success(res));
+    } catch (e) {
+        yield put(deleteArticle.failure());
+    }
 }
 
 const updateArticleApi = (
-  token: any,
-  id: number,
-  payload: UpdatedArticleInfo
+    token: any,
+    id: number,
+    payload: UpdatedArticleInfo
 ) =>
-  axios.patch(HOST + `/articles/${id}/`, payload, {
-    headers: { Authorization: `token ${token}` },
-  });
+    axios.patch(HOST + `/articles/${id}/`, payload, {
+        headers: { Authorization: `token ${token}` },
+    });
 
 function* updateArticleAsync(action: {
-  type: string;
-  payload: UpdatedArticleInfo;
+    type: string;
+    payload: UpdatedArticleInfo;
 }) {
-  try {
-    const token = localStorage.getItem('token');
-    const id = action.payload.id;
+    try {
+        const token = localStorage.getItem('token');
+        const id = action.payload.id;
 
-    const res = yield call(updateArticleApi, token, id, action.payload);
-    yield put(updateArticle.success(res.data));
-  } catch (e) {
-    yield put(updateArticle.failure('업데이트 실패'));
-  }
+        const res = yield call(updateArticleApi, token, id, action.payload);
+        yield put(updateArticle.success(res.data));
+    } catch (e) {
+        yield put(updateArticle.failure('업데이트 실패'));
+    }
 }
 
 export function* watchArticle() {
-  yield takeEvery(CREATE_ARTICLE_REQUEST, createArticleAsync);
-  yield takeEvery(FETCH_ARTICLE_LIST_REQUEST, fetchArticleListAsync);
-  yield takeEvery(UPDATE_ARTICLE_REQUEST, updateArticleAsync);
-  yield takeEvery(DELETE_ARTICLE_REQUEST, deleteArticleAsync);
+    yield takeEvery(CREATE_ARTICLE_REQUEST, createArticleAsync);
+    yield takeEvery(FETCH_ARTICLE_LIST_REQUEST, fetchArticleListAsync);
+    yield takeEvery(UPDATE_ARTICLE_REQUEST, updateArticleAsync);
+    yield takeEvery(DELETE_ARTICLE_REQUEST, deleteArticleAsync);
 }
