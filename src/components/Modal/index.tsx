cd2b82d3bcb49d77648fca918f5d73c1bc7e5453
issol <isolatorv@gmail.com>
2021-03-08 11:46:09 +0900

    refac / modal css

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -48,18 +48,16 @@ const Overlay = styled.div`
 const Contents = styled.div`
   box-sizing: border-box;
   position: relative;
-  top: 0px;
-  padding: 0 auto;
   border-radius: 10px;
   box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
   background-color: #fff;
   text-align: center;
   width: 360px;
   max-width: 480px;
-  height: 300px;
+  height: 250px;
   transform: translateY(-50%);
   margin: 0 auto;
-  padding: 40px 20px;
+  padding: 60px 20px;
 `;
 
 export default Modal;
--- src/components/StatusModal/index.tsx
+++ src/components/StatusModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from 'components/Modal';
 import React from 'react';
+import styled from 'styled-components';
 
 type Props = {
   isOpenStatusModal: boolean;
@@ -12,9 +13,23 @@ const StatusModal = ({ isOpenStatusModal, setIsOpenStatusModal, statusMessage, o
   return (
     <Modal isOpen={isOpenStatusModal} setIsOpen={setIsOpenStatusModal}>
       <div>{statusMessage}</div>
-      <button onClick={onClose}>close</button>
+      <CloseStatusModalButton onClick={onClose}>close</CloseStatusModalButton>
     </Modal>
   );
 };
+
+const CloseStatusModalButton = styled.button`
+  width: 50%;
+  padding: 10px 30px;
+  margin: 30px auto 0 auto;
+
+  cursor: pointer;
+  display: block;
+
+  background: linear-gradient(to right, #ff105f, #ffad06);
+  border: 0;
+  outline: none;
+  border-radius: 30px;
+`;
 
 export default StatusModal;
